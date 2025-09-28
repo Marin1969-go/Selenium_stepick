a190b74935b73bf0582e7b6d17bf7f71c04a9459
marin <morr.marin@yandex.ru>
2025-09-28 17:55:47 +0300

    def_ddd+mmm1

--- second/second.py
+++ second/second.py
@@ -2,6 +2,7 @@
 import math as m
 
 def fff(a,d):
+
     return a+d
 def ddd(a,d):
     return a**d
@@ -12,3 +13,4 @@ if __name__ == '__main__':
     print(fff(2,2))
     print(ddd(2222,2))
     print(mmm(3,2))
+    print(list(map(fff, [1,2,3,4,1], [4,6,8,9,0,8])))
