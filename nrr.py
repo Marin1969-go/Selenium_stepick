44f71157d030af1ab095ecd37995b7823c4563fa
Marin1969-go <morr.marin@yandex.ru>
2025-09-28 19:12:04 +0300

    NNr

---
+++ nrr.py
@@ -0,0 +1,9 @@
+from second import *
+from selenium import webdriver
+
+browser = webdriver.Chrome()
+browser.get('https://google.com')
+print(browser.page_source)
+ if __name__ == '__main__':
+
+     print(fff(3,6))
