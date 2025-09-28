0543f738cc388e20aa88d6fd908699364f25e09c
marin <morr.marin@yandex.ru>
2025-09-28 17:46:42 +0300

    def_ddd+mmm

--- second/second.py
+++ second/second.py
@@ -1,7 +1,14 @@
+
+import math as m
+
 def fff(a,d):
     return a+d
 def ddd(a,d):
     return a**d
+def mmm(a,d):
+    return m.sin(a*d)
+
 if __name__ == '__main__':
-    print(fff([1,2,3,4,5],2))
-    print(ddd([1,2,3,4,5],2))
+    print(fff(2,2))
+    print(ddd(2222,2))
+    print(mmm(3,2))
---
+++ second/third/upper.py
@@ -0,0 +1,2 @@
+def Up(text):
+    return text.upper()
